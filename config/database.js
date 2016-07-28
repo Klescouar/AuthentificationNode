5b7568068084e38b1097b6fc967023723162c8d8
Le scouarnec Kevin <kevin_lescouarnec@DX-CZC14056XD.groupevsc.com>
2016-07-28 13:26:35 +0200

    Template SignUp/SignIn

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb://Poncho:mlkPOIlkjOIU098@ds029585.mlab.com:29585/node-project-mongo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
